e3f691f1d349e704fe9e4f8d37b284bb74160dd4
skipperguy12 <skipperguy12@users.noreply.github.com>
2015-01-15 18:52:24 -0500

    Remove duplicate user method in dummy application controller

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -5,12 +5,6 @@ class ApplicationController < ActionController::Base
   end
   helper_method :mongoid_forums_user
 
-  def mongoid_forums_user
-    current_user
-  end
-
-  helper_method :mongoid_forums_user
-
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
