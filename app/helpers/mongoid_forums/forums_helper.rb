62a8c3b8555f5547e1d11054b53735bc5f878a1c
skipperguy12 <skipperguy12@users.noreply.github.com>
2015-01-15 17:40:16 -0500

    Add posts count caching to forum

--- app/helpers/mongoid_forums/forums_helper.rb
+++ app/helpers/mongoid_forums/forums_helper.rb
@@ -1,4 +1,15 @@
 module MongoidForums
   module ForumsHelper
+    def topics_count(forum)
+      forum.topics.count
+    end
+
+    def posts_count(forum)
+      if forum.posts_count == nil
+        forum.posts_count = forum.topics.inject(0) {|sum, topic| topic.posts.count + sum }
+        forum.save
+      end
+      forum.posts_count
+    end
   end
 end
--- app/models/mongoid_forums/forum.rb
+++ app/models/mongoid_forums/forum.rb
@@ -6,9 +6,25 @@ module MongoidForums
     belongs_to :category, :class_name => "MongoidForums::Category"
     has_many :topics, :class_name => "MongoidForums::Topic"
 
+    # Caching
+    field :posts_count, :type => Integer
+
     field :name
     validates :name, :presence => true
 
     field :order, :type => Integer, :default => 0
+
+    def count_of_posts
+      topics.inject(0) {|sum, topic| topic.posts.count + sum }
+    end
+
+    def increment_posts_count
+      if self.posts_count == nil
+        self.posts_count = 1
+      else
+        self.posts_count += 1
+      end
+      self.save
+    end
   end
 end
