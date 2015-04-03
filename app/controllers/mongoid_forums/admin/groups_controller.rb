34ff80633872d3a82eb4f284de4145157ee0a7ac
skipperguy12 <skipperguy12@users.noreply.github.com>
2015-04-03 16:44:50 -0400

    Begin work on UI for group management

--- app/controllers/mongoid_forums/admin/groups_controller.rb
+++ app/controllers/mongoid_forums/admin/groups_controller.rb
@@ -1,66 +1,65 @@
 require_dependency "mongoid_forums/application_controller"
 
 module MongoidForums
-  class Admin::GroupsController < ApplicationController
+  module Admin
+    class GroupsController < BaseController
 
-    before_action :set_group, except: [:index, :new, :create]
-
-    def index
-      @groups = Group.all
-    end
-
-    def new
-      @group = Group.new
-    end
-
-    def create
-      if @group = Forum.create(group_params)
-        flash[:notice] = "Group created successfully"
-        redirect_to @group
-      else
-        flash.now.alert = "Group could not be created"
-        render :action => "new"
+      def index
+        @groups = Group.all
       end
+
+      def new
+        @group = Group.new
+      end
+
+      def create
+        if @group = Group.create(params.require(:group).permit(:name, :members))
+          flash[:notice] = "Group created successfully"
+          redirect_to [:admin, @group]
+        else
+          flash.now.alert = "Group could not be created"
+          render :action => "new"
+        end
+      end
+
+      def edit
+        @group = Group.find(params[:id])
+      end
+
+      def update
+        @group = Group.find(params[:id])
+      end
+
+      def show
+        @group = Group.find(params[:id])
+      end
+
+      def destroy
+        @group.destroy
+      end
+
+      ### Temporary Methods - Try Not To Cringe Too Much <3 ###
+      def add_member
+        user = User.find(params.require(:user_id))
+
+        group.members << user.id
+        group.save
+      end
+
+      def remove_member
+        group = Group.find(params.require(:group_id))
+        user = User.find(params.require(:user_id))
+
+        group.members.delete(user.id)
+        group.save
+      end
+      #########################################################
     end
 
-    def edit
+    private
+
+    def group_params
+      params.require(:group).permit(:name, :members)
     end
-
-    def update
-    end
-
-    def show
-    end
-
-    def destroy
-      @group.destroy
-    end
-
-    ### Temporary Methods - Try Not To Cringe Too Much <3 ###
-    def add_member
-      user = User.find(params.require(:user_id))
-
-      group.members << user.id
-      group.save
-    end
-
-    def remove_member
-      group = Group.find(params.require(:group_id))
-      user = User.find(params.require(:user_id))
-
-      group.members.delete(user.id)
-      group.save
-    end
-    #########################################################
-  end
-
-  private
-
-  def group_params
-    params.require(:group).permit(:name, :members)
-  end
-
-  def set_group
-    @group = Group.find(params.require(:group_id))
   end
 end
