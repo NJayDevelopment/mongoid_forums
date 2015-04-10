84563d75bf3fdc4a0fb307e8a1a5f7ddedeaa3c3
skipperguy12 <skipperguy12@users.noreply.github.com>
2015-04-10 18:58:21 -0400

    Update mongoid_forums.rb
    Thanks for the catch @AustinLMayes

--- lib/mongoid_forums.rb
+++ lib/mongoid_forums.rb
@@ -37,7 +37,7 @@ module MongoidForums
     def user_class
       if @@user_class.is_a?(Class)
         raise "You can't set MongoidForums.user_class to be a class. Please use a string instead.\n\n " +
-              "See https://github.com/radar/mongoid_forums/issues/88 for more information."
+              "See https://github.com/radar/forem/issues/88 for more information."
       elsif @@user_class.is_a?(String)
         begin
           Object.const_get(@@user_class)
